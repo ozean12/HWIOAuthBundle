9cf450a7e93538808664c2bd4255cb3998c3177a
Nikolai Besschetnov <nikolai@ozean12.com>
2017-01-16 17:48:18 +0100

    Adds the Figo resource owner

---
+++ OAuth/ResourceOwner/FigoResourceOwner.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FigoResourceOwner extends GenericOAuth2ResourceOwner
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $paths = array(
+        'identifier' => 'user_id',
+        'nickname' => 'name',
+        'realname' => 'name',
+        'email' => 'email',
+    );
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'authorization_url' => 'https://api.figo.me/auth/code',
+            'access_token_url' => 'https://api.figo.me/auth/token',
+            'infos_url' => 'https://api.figo.me/rest/user',
+            'use_bearer_authorization' => false,
+        ));
+    }
+}
---
+++ Tests/OAuth/ResourceOwner/FigoResourceOwnerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HWI\Bundle\OAuthBundle\Tests\OAuth\ResourceOwner;
+
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\FigoResourceOwner;
+
+class FigoResourceOwnerTest extends GenericOAuth2ResourceOwnerTest
+{
+    protected $userResponse = <<<json
+{
+    "user_id": "1",
+    "name": "bar",
+    "email": "baz"
+}
+json;
+
+    protected $paths = array(
+        'identifier' => 'user_id',
+        'nickname' => 'name',
+        'realname' => 'name',
+        'email' => 'email',
+    );
+
+    protected $expectedUrls = array(
+        'authorization_url' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+        'authorization_url_csrf' => 'http://user.auth/?test=2&response_type=code&client_id=clientid&state=random&redirect_uri=http%3A%2F%2Fredirect.to%2F',
+    );
+
+    protected function setUpResourceOwner($name, $httpUtils, array $options)
+    {
+        return new FigoResourceOwner($this->buzzClient, $httpUtils, $options, $name, $this->storage);
+    }
+}
